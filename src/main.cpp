30d7d1c8ad1d235e9e2096a24f6bb499be7cef48
Hueter <kohsukemori@gmail.com>
2024-09-18 00:39:56 +0900

    各モータの変数用意

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,19 @@
 #include <MotorDriver.hpp>
 #include <ak\Ps3State.hpp>
 
-void setup() {
+MotorDriver omniMotor1;
+MotorDriver omniMotor2;
+MotorDriver omniMotor3;
+
+MotorDriver firingMotor;
+MotorDriver firingDirectionMotor;
+Servo       pushBollServo;
+
+Servo grabbingServo;
+Servo liftingServo;
+
+
+void  setup() {
 }
 
 void loop() {
