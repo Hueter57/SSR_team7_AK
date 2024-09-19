3ef0151092bea7a31714958ce723d77af20fbdf8
Chikurin1014 <chikurin1014@gmail.com>
2024-09-19 16:46:45 +0900

    main_cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <Arduino.h>
 
+#include "ak/Device.hpp"
+#include "ak/Input.hpp"
+#include "ak/Output.hpp"
+#include "ak/System.hpp"
+
+ak::Device device;
+ak::System system;
+
 void setup() {
+    device.setup();
+    system.set_forward_launcher(true, true);
+    system.set_forward_stearing(true, true, true);
+    system.set_pulse_width_arm(0, 0, 0, 0);
+    system.set_pulse_width_launcher(0, 0);
 }
 
 void loop() {
+    auto input  = device.get_input();
+    auto output = system.update(input);
+    device.set_value(output);
 }
